24606d690dacd159f8b50b7c15ee5427c364d702
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-12 22:42:46 -0600

    se agregan los valores por defecto para ListaNodo

--- 1/twoNumbers.java
+++ 1/twoNumbers.java
@@ -6,7 +6,13 @@ public class twoNumbers {
 
     //puede ir en un paquete aparte- esta clase permitirá crear la lista de los nodos
     class ListaNodo {
-        
-        
+        //Representa un nodo de la lista enlazada 
+        //con un valor entero val y una referencia next
+        int val;
+        ListaNodo next;
+
+        ListaNodo() {}
+        ListaNodo(int val) { this.val = val; }
+        ListaNodo(int val, ListaNodo next) { this.val = val; this.next = next; }            
     }
 }
