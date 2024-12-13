555a594555f4b93876630295b906bb30287d377e
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-12 18:15:04 -0600

    se importa la libreria de escanner y se pide al usuario que ingrese datos

--- 3/validParenthesis.java
+++ 3/validParenthesis.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class validParenthesis {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa los siguientes parejas de caracteres: () {} []");
+        String s = scanner.nextLine();
+
     }
-    
+
 }
