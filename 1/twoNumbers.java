6000e74db08f4f9864830a68916f1b376421e424
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-12 22:37:06 -0600

    se modifica el nombre de la clase y se agrega una clase secundaria de nombre ListaNodo

---
+++ 1/twoNumbers.java
@@ -0,0 +1,12 @@
+public class twoNumbers {
+
+    public static void main(String[] args) {
+        
+    }
+
+    //puede ir en un paquete aparte- esta clase permitirá crear la lista de los nodos
+    class ListaNodo {
+        
+        
+    }
+}
