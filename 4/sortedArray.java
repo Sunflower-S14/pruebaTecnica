d6e2188d8a285f99ecb0ec9e5af8ea76cbcf3382
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-14 12:49:09 -0600

    se crea la carpeta junto con el ejercicio 4

---
+++ 4/sortedArray.java
@@ -0,0 +1,16 @@
+public class sortedArray {
+    
+    //método principal
+    public static void main(String[] args) {
+        
+    }
+
+
+}
+
+//se crea una clase que tendrá la lógica de solución
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+    }
+}
