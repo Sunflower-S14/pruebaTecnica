101f48efa4f52301d42c1abc5ed8fc884c110e44
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-16 00:29:28 -0600

    se distribuye en una clase nueva el método lengthOfLongestSubstring

--- 2/calculadoraCadenas.java
+++ 2/calculadoraCadenas.java
@@ -1,9 +1,18 @@
 public class calculadoraCadenas {
     public static void main(String[] args) {
-        
+
+        //Creamos el objeto que llamará a lengthOfLongestSubstring
+        Solution solucion = new Solution();
+        System.out.println(solucion.lengthOfLongestSubstring("abcabcbb"));
     }
+ 
+}
+
+class Solution {
 
     public int lengthOfLongestSubstring(String s) {
         
     }
+
+    
 }
