10bd003b52d7b1fc47bdc3ed952f3577d43e932f
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-15 22:34:51 -0600

    en el main se agregarán los datos que se pasarán al metodo strStr

--- 5/indexString.java
+++ 5/indexString.java
@@ -2,6 +2,13 @@ public class indexString {
     
     public static void main(String[] args) {
         
+        // Ejemplo 1
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        //Creamos el objeto para usar el método strStr
+        Solution solucion = new Solution();
+        System.out.println("Output: " + solucion.strStr(haystack, needle)); // Salida: 0
     }
 }
 
