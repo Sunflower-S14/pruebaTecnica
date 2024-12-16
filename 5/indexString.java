0aded659d0fcfa4a6098536fa868cf3dee717aca
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-15 22:29:14 -0600

    se crean los métodos a usar

--- 5/indexString.java
+++ 5/indexString.java
@@ -1,3 +1,14 @@
 public class indexString {
     
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+    }
+    
 }
