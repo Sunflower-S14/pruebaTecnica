f34dccddbdb8a99f20f33994e78cce26bad1cf2b
Sunflower-S14 <0.atcsn@gmail.com>
2024-12-16 00:05:06 -0600

    se agrega la entrada de datos en la terminal

--- 5/indexString.java
+++ 5/indexString.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class indexString {
     
     public static void main(String[] args) {
         
-        // Ejemplo 1
-        String haystack = "sadbutsad";
-        String needle = "sad";
+        //Se agrega la lectura de datos en ls terminal
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Ingresa el texto en minúsculas: ");
+        String haystack = scanner.nextLine();
+
+        System.out.print("Ingresa el patrón a buscar: ");
+        String needle = scanner.nextLine();
+
+        scanner.close();
+        // Ejemplo 1
+        //String haystack = "sadbutsad";
+        //String needle = "sad";
+
+        //Ejemplo 2
+        //String haystack = "leetcode";
+        //String needle = "leeto";
+        
         //Creamos el objeto para usar el método strStr
         Solution solucion = new Solution();
         System.out.println("Output: " + solucion.strStr(haystack, needle)); // Salida: 0
